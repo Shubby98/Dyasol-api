a774f1cbf7ff515f9c2b09a049a6ef3b170abfff
Shubby <Shubby98@gmail.com>
2019-09-22 02:16:49 +0530

    TextToSpeech chg

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "Welcome to our API" 
-"""
+
 @app.route('/texttospeech/' , methods = ['POST'])
 def texttospeech():
 	if request.method == 'POST':
-		return Response(response={"apple" : "iphone 11"} ,status = 200)
-"""
+		return Response(response={"apple" : "iphone 11"} ,status = 200 ,  mimetype="application/json")
+
 if __name__ == '__main__':
 	app.run()
