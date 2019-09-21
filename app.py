ecaa113acd64001c411d09b542754a52271b2ffe
Shubby <Shubby98@gmail.com>
2019-09-22 02:13:47 +0530

    TextToSpeech chg

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask , request , Response
 #import gtts
 #from pygame import mixer
 
@@ -8,11 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "Welcome to our API" 
-
-@app.route('/texttospeech/' , methods = ['PUT'])
+"""
+@app.route('/texttospeech/' , methods = ['POST'])
 def texttospeech():
-	if request.method == 'PUT':
-		return jsonify({"dowloaded" : request.files['img1']}
-
+	if request.method == 'POST':
+		return Response(response={"apple" : "iphone 11"} ,status = 200)
+"""
 if __name__ == '__main__':
 	app.run()
