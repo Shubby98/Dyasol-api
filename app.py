e3d23f4a3da0997bbcb9dbc05b089993c7246539
Shubby <Shubby98@gmail.com>
2019-09-22 00:07:02 +0530

    runtime file added

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "Welcome to our API" 
-
+"""
 @app.route('/texttospeech/' , methods = ['PUT'])
 def texttospeech():
 	if request.method == 'PUT':
 		filename = images.save(request.file['img1'])
 		return filename
+"""
 
 if __name__ == '__main__':
 	app.run(port =  7000 , debug = True ,use_reloader = True)
