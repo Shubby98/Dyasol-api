f8afa1884c0c0b1999016e2f7dc7e94f3036d6e2
Shubby <Shubby98@gmail.com>
2019-09-22 01:46:25 +0530

    TextToSpeech content changed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
 @app.route('/texttospeech/' , methods = ['PUT'])
 def texttospeech():
 	if request.method == 'PUT':
-		return "cool"
+		return request.files.keys()
 
 if __name__ == '__main__':
 	app.run()
